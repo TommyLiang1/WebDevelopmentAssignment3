a573b5788cfcbd98b0511cc31efb754e0c32ee6e
TommyLiang1 <tommyliang131@gmail.com>
2022-10-20 18:21:18 -0400

    reset main to starter code

--- script.js
+++ script.js
@@ -5,75 +5,12 @@ let colorSelected;
 
 // Add a row
 function addR() {
-    
-// td is used for columns while tr is used for rows
-let column = document.createElement("td");
-let row = document.createElement("tr");
-let totalGrid = document.getElementById("grid"); // getting data from our table id grid in index.html
-
-    // If there are no rows it will add one
-    if(numCols === 0){
-        row.appendChild(document.createElement("td"));
-        totalGrid.appendChild(row);
-
-            if(numRows === 0){
-                numCols++;
-            }
-        }
-    // If there are already existing rows and columns then add to the existing
-    else{
-        for(let i = 0; i < numCols; i++){
-            row.appendChild(document.createElement("td"));
-        }
-        totalGrid.appendChild(row);
-    }
-
-// add to the count of rows
-numRows++;
-// allows clicked boxes to change to specified color (tried at top but will always be one row/col off unless declared later on)
-let colorClick = document.querySelectorAll("tr td");
-
-for(let i = 0; i < colorClick.length; i++){
-    colorClick[i].onclick = function(){
-    this.style.backgroundColor = colorSelected;
-    }
-}
-
+    alert("Clicked Add Row"); // Replace this line with your code.
 }
 
 // Add a column
 function addC() {
-   
-// td is used for columns while tr is used for rows
-let column = document.createElement("td");
-let row = document.createElement("tr");
-let totalGrid = document.getElementById("grid"); // getting data from our table id grid in index.html
-let prevRows = document.querySelectorAll("tr"); //checking if there are already rows to add alongside them
-
-if(numRows === 0){
-    row.appendChild(document.createElement("td"));
-    totalGrid.appendChild(row);
-    
-        if(numCols === 0){
-            numRows++;
-        }
-    }
-    else{
-        for(let i = 0; i < numRows; i++){
-            prevRows[i].appendChild(document.createElement("td"));
-        }
-    }
-    // add to the count of columns
-    numCols++;
-
-    // allows clicked boxes to change to specificied color
-    let colorClick = document.querySelectorAll("tr td"); 
-
-    for(let i = 0; i < colorClick.length; i++){
-        colorClick[i].onclick = function(){
-        this.style.backgroundColor = colorSelected;
-    }
-}
+    alert("Clicked Add Col"); // Replace this line with your code.
 }
 
 // Remove a row
